2c02c8e220249eddf434d6324d5d91dadc1c5938
Subhash Mehta <subhashkumar746310@gmail.com>
2025-09-24 00:45:00 +0530

    remove logo

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -15,11 +15,6 @@ const TwitterIcon = () => (
     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><path d="M23 3a10.9 10.9 0 0 1-3.14 1.53 4.48 4.48 0 0 0-7.86 3v1A10.66 10.66 0 0 1 3 4s-4 9 5 13a11.64 11.64 0 0 1-7 2c9 5 20 0 20-11.5a4.5 4.5 0 0 0-.08-.83A7.72 7.72 0 0 0 23 3z"></path></svg>
 );
 
-const LoginIcon = () => (
-  <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor" aria-hidden="true">
-    <path d="M12 2a5 5 0 1 1 0 10 5 5 0 0 1 0-10Zm0 12c4.418 0 8 2.239 8 5v1H4v-1c0-2.761 3.582-5 8-5Z"/>
-  </svg>
-);
 
 
 function Hero() {
@@ -65,9 +60,6 @@ function Hero() {
             <a href="https://twitter.com/your-username" target="_blank" rel="noopener noreferrer" aria-label="Twitter Profile">
               {/* <-- Apna Twitter/X link daalein */}
               <TwitterIcon />
-            </a>
-            <a href="#login" aria-label="Login">
-              <LoginIcon />
             </a>
           </div>
 
